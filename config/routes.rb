6a7ec2411df813b51e13ec898b5ddc1d15d04e3b
Megan Thresh <meganthresh96@gmail.com>
2020-05-13 22:33:13 +0100

    committing app with all major functions completed. Still need to fix a bug with function to delete reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants do 
-    resources :reviews, shallow: true
-  end 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   resources :restaurants do
+     resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
 end
